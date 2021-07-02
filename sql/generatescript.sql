4ea07db4717b2b8573d0e20bb8dc134269ee6657
overjoyedbrass <79214190+overjoyedbrass@users.noreply.github.com>
2021-07-02 13:29:55 +0200

    Update generatescript.sql

--- sql/generatescript.sql
+++ sql/generatescript.sql
@@ -3,22 +3,22 @@ truncate users, documents, clicks cascade;
 
 insert into users (id, name)
 select	i, 'User' || i 
-from generate_series(1, 1000) as seq(i);
+from generate_series(1, 100000) as seq(i);
 
 
 insert into documents (id, name)
 select	i, 'Doc' || i 
-from generate_series(1, 1000) as seq(i);
+from generate_series(1, 100000) as seq(i);
 
 
 create or replace function random_user() returns UUID language sql as 
 $$ 
-SELECT uuid FROM users WHERE id > random()*1000 limit 1
+SELECT uuid FROM users TABLESAMPLE SYSTEM_ROWS(1)
 $$;
 
 create or replace function random_document() returns UUID language sql as 
 $$
-SELECT uuid FROM documents WHERE id > random()*1000 limit 1
+SELECT uuid FROM documents TABLESAMPLE SYSTEM_ROWS(1)
 $$;
 
 
